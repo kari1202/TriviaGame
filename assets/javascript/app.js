4f9fcd5ff43280b947e52e24c440e6400723a762
Kari <karinan1202@gmail.com>
2019-03-08 18:50:21 -0600

    added images and initial styling

---
+++ assets/javascript/app.js
@@ -0,0 +1,54 @@
+
+//Questions and answers
+var triviaArr = [
+    {
+        question: "What type of farm does Dwight own?",
+        choices: ["Bear farm", "Carrot farm", "Beet farm", "Beetle farm"],
+        answer: 2
+    },
+    {
+        question: "What name did Pam and Angela fight over for their babies?",
+        choices: ["Phillip", "Benjamin", "Andrew", "James"],
+        answer: 0
+    },
+    {
+        question: "Where do Jim and Pam share their first real kiss?",
+        choices: ["The roof", "Jim's desk", "The warehouse", "The parking lot"];
+        answer: 1
+    },
+    {
+        question: "Which of Angela's cats does Dwight freeze?",
+        choices: ["Bandit", "Sparkles", "Sprinkles", "Fluffy"],
+        answer: 2
+    },
+    {
+        question: "What tatoo is Andy forced to get?",
+        choices: ["A wolf", "A nard dog", "A naked man", "The Cornell logo"],
+        answer: 1
+    },
+    {
+        question: "Which office member was having an affair with Angela's fiance, the senator?",
+        choices: ["Andy", "Kelly", "Erin", "Oscar"],
+        answer: 3
+    },
+    {
+        question: "What was Plan A for Jim's wedding?",
+        choices: ["Marrying her a long time ago, pretty much the day he met her", "Vegas", "Marrying her after the baby was born", "A small beach wedding"],
+        answer: 0
+    },
+    {
+        question: "Who ruined Pam's pregnancy secret during the wedding weekend?",
+        choices: ["Andy", "Michael", "Holly", "Jim"],
+        answer: 3
+    },
+    {
+        question: "What is Michael's username for the online dating website?",
+        choices: ["Ready for marriage", "Kid crazy", "Little kid lover", "Looking for woman"],
+        answer: 2
+    },
+    {
+        question: "What type of car does Dwight drive?",
+        choices: ["Corvette", "Trans Am", "Camaro", "Prius"],
+        answer: 1
+    }
+]
